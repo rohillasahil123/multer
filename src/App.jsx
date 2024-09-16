a8badd1a195d4bbfe80a3fc0ec9c2be81439c5f1
rohillasahil123 <rohillasahil704@gmail.com>
2024-09-16 18:31:58 +0530

    p

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+<div className='text-center items-center text-bold text-3xl mx-auto '>
+     <h1> Welcome</h1>
+    </div>
+  )
+}
+
+export default App
